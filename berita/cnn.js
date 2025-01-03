c078e31c188850bdfaa2ecc11e75922a05190d4f
alfinofc1 <160604519+alfinofc1@users.noreply.github.com>
2025-01-04 01:17:01 +0700

    Add files via upload

--- berita/cnn.js
+++ berita/cnn.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 module.exports = function(app) {
 
   // Fungsi scraper liputan6
-  async function liputan6() {
+  async function cnn() {
     try {
       const AvoskyBaik = await axios.get('https://mdsay.xyz/api/v1?key=md&api=cnn-nasional');
       const $ = cheerio.load(AvoskyBaik.data);
@@ -15,13 +15,14 @@ module.exports = function(app) {
       latestNews.each(function () {
         try {
           const berita = $(this).find('figure a').attr('berita');
-          const link = $(this).find('figure a').attr('href');
+          const title = $(this).find('figure a').attr('title');
+          const date = $(this).find('figure a').attr('date');
           const thumbnail = $(this).find('figure a picture thumbnail').attr('data-src');
-          const caption = $(this).find('aside header a').text();
+          const caption = $(this).find('figure a').attr('caption');
           const url = $(this).find('figure a').attr('url')
           const text = $(this).find('figure a').attr('text')
 
-          results.push({ berita, link, thumbnail, caption, url, text, source: 'cnn' });
+          results.push({ berita, title, date, thumbnail, caption, url, text, source: 'cnn' });
         } catch (e) {
           console.error('Error scraping article:', e);
         }
